252b1fa6b2aa1bc4d776a2da0282fd1abeb319a8
Glitch (javascript-array-sort) <none>
2018-11-30 10:46:54 +0000

    📉🚃 Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# javascript-array-sort
-Javascript30 tutorial 17 from Wes Bos. Practise arrays sort methods.
+Welcome to Glitch
+=================
+
+Javascript30 tutorial 17 from Wes Bos. Practise array sort method,  using ES6 javascript and a mapping function.
+
+
+### To Do
+
+- [ ] try longer lists with more article combinations and change regex. 
+- [ ] add functionality
+
+
+### Notes
+
+link to Youtube video:
+https://www.youtube.com/watch?v=PEEo-2mRQ7A&index=17&list=PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH
---
+++ script.js
@@ -0,0 +1,22 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 
+               'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 
+               'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim(); //note space after articles in regex expression
+}
+
+const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1); 
+
+document.querySelector('#bands').innerHTML = 
+  sortedBands
+    .map(bands => `<li>${bands}</li>`)
+    .join(''); //to join into one big string, not lots of individual strings.
+
+console.log(sortedBands);
+
+/*Notes
+The sort order can be either alphabetic or numeric, and either ascending (up) or descending (down).
+By default, the sort() method sorts the values as strings in alphabetical and ascending order.
+*/
+

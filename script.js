6187aebd2ee17ed06abb7c2b057cd848accddace
AndrewJBateman <gomezbateman@yahoo.com>
2022-03-25 10:06:22 +0100

    styles: linting, list colors changed

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 
-               'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 
-               'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog",
+];
 
 // replace articles a, an & the with nothing, note space after articles in regex expression
 function strip(bandName) {
-  return bandName.replace(/^(a |the |an )/i, '').trim();
+  return bandName.replace(/^(a |the |an )/i, "").trim();
 }
 
-// sort in alphabetical order 
-const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1); 
+// sort in alphabetical order
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
 
 // list sortedbands using original bands array names.
-document.querySelector('#bands').innerHTML = 
-  sortedBands
-    .map(bands => `<li>${bands}</li>`)
-    .join(''); //to join into one big string, not lots of individual strings.
+document.querySelector("#bands").innerHTML = sortedBands
+  .map((bands) => `<li>${bands}</li>`)
+  .join(""); //to join into one big string, not lots of individual strings.
 
 console.log(sortedBands);
 
